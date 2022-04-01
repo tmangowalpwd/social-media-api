0055f4f4c3021adf51e4190ea063faf2d1e715d3
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-04-01 11:27:50 +0700

    upload content

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -16,23 +16,24 @@ router.get("/", async (req, res) => {
       },
       limit: _limit ? parseInt(_limit) : undefined,
       offset: (_page - 1) * _limit,
-      include: [
-        {
-          model: User,
-          attributes: ["username"]
-        },
-        {
-          // Get users who liked the posts
-          // Use nested many-to-many because if we 
-          // include the User model directly, the 1-to-many
-          // relationship is going to be selected instead (user_id in Post)
-          model: Like,
-          include: User
-        }
-      ],
+      include: User
+      // include: [
+      //   {
+      //     model: User,
+      //     attributes: ["username"]
+      //   },
+      // {
+      // Get users who liked the posts
+      // Use nested many-to-many because if we 
+      // include the User model directly, the 1-to-many
+      // relationship is going to be selected instead (user_id in Post)
+      // model: Like,
+      // include: User
+      // }
+      // ],
       // To prevent wrong row count when
       // querying/including many-to-many data
-      distinct: true
+      // distinct: true
     })
 
     return res.status(200).json({
@@ -56,7 +57,7 @@ router.post("/",
     .single("post_image_file"),
   async (req, res) => {
     try {
-      const { caption, location } = req.body;
+      const { caption, location, user_id } = req.body;
 
       const uploadFileDomain = process.env.UPLOAD_FILE_DOMAIN
       const filePath = "post_images"
@@ -65,7 +66,8 @@ router.post("/",
       const newPost = await Post.create({
         image_url: `${uploadFileDomain}/${filePath}/${filename}`,
         caption,
-        location
+        location,
+        user_id
       })
 
       return res.status(201).json({
